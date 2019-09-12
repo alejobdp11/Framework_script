95d3ce2b7acf1e27a7538a457eee6b7bba242826
alejobdp11 <50469637+alejobdp11@users.noreply.github.com>
2019-09-11 19:47:32 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -6,37 +6,40 @@ $( document ).ready(function() {
     $(".btn-reinicio").replaceWith("<button class='btn-reinicio'>Reiniciar</button>")
     for (var i = 0; i <6; i++) {
       var num=Math.round(Math.random() * (4 - 1) + 1);;
-      $(".col-1").append('<img class="soltar" id="'+num+'" src="C:/Users/DavidAlejandroSilvaL/Desktop/next_u/frameworks_jscript/final/image/'+num+'.png" alt="">');
+      $(".col-1").append('<img class="soltar" id="'+num+'" src="image/'+num+'.png" alt="">');
     }
     for (var i = 0; i <6; i++) {
       var num=Math.round(Math.random() * (4 - 1) + 1);;
-      $(".col-2").append('<img class="soltar" id="'+num+'" src="C:/Users/DavidAlejandroSilvaL/Desktop/next_u/frameworks_jscript/final/image/'+num+'.png" alt="">');
+      $(".col-2").append('<img class="soltar" id="'+num+'" src="image/'+num+'.png" alt="">');
     }
     for (var i = 0; i <6; i++) {
       var num=Math.round(Math.random() * (4 - 1) + 1);;
-      $(".col-3").append('<img class="soltar" src="C:/Users/DavidAlejandroSilvaL/Desktop/next_u/frameworks_jscript/final/image/'+num+'.png" alt="">');
+      $(".col-3").append('<img class="soltar" id="'+num+'" src="image/'+num+'.png" alt="">');
     }
     for (var i = 0; i <6; i++) {
       var num=Math.round(Math.random() * (4 - 1) + 1);;
-      $(".col-4").append('<img class="soltar" src="C:/Users/DavidAlejandroSilvaL/Desktop/next_u/frameworks_jscript/final/image/'+num+'.png" alt="">');
+      $(".col-4").append('<img class="soltar" id="'+num+'" src="image/'+num+'.png" alt="">');
     }
     for (var i = 0; i <6; i++) {
       var num=Math.round(Math.random() * (4 - 1) + 1);;
-      $(".col-5").append('<img class="soltar" src="C:/Users/DavidAlejandroSilvaL/Desktop/next_u/frameworks_jscript/final/image/'+num+'.png" alt="">');
+      $(".col-5").append('<img class="soltar" id="'+num+'" src="image/'+num+'.png" alt="">');
     }
     for (var i = 0; i <6; i++) {
       var num=Math.round(Math.random() * (4 - 1) + 1);;
-      $(".col-6").append('<img class="soltar" src="C:/Users/DavidAlejandroSilvaL/Desktop/next_u/frameworks_jscript/final/image/'+num+'.png" alt="">');
+      $(".col-6").append('<img class="soltar" id="'+num+'" src="image/'+num+'.png" alt="">');
     }
     for (var i = 0; i <6; i++) {
       var num=Math.round(Math.random() * (4 - 1) + 1);;
-      $(".col-7").append('<img class="soltar" src="C:/Users/DavidAlejandroSilvaL/Desktop/next_u/frameworks_jscript/final/image/'+num+'.png" alt="">');
+      $(".col-7").append('<img class="soltar" id="'+num+'" src="image/'+num+'.png" alt="">');
     }
-
+    //cambio de color del titulo
     $(".main-titulo").css("color","blue");
-    //cuenta atras-----------------
+    //mover elementos-----------------
     $(function() {
+      $(".main-titulo").css("color","blue");
         $(".soltar" ).draggable();
-
+        $(".main-titulo").css("color","yellow");
     });
+    //comprobar las figuras
+    var valores
     });
